afa8ef0d632e49e3dd98752b4abbdbacf381f1b3
Another <joseluismanco37@gmail.com>
2024-03-09 23:43:17 -0500

    feat: deploy contracts

--- packages/smart_contracts/hardhat.config.ts
+++ packages/smart_contracts/hardhat.config.ts
@@ -6,11 +6,17 @@ const config: HardhatUserConfig = {
   networks: {
     echo: {
       url: 'https://subnets.avax.network/echo/testnet/rpc',
-      chainId: 173750
+      chainId: 173750,
+      accounts: [
+        '0xd02eea6052529458cca873975cf6b226a92282758784a2e06d6733aa45ea47b9'
+      ]
     },
     'c-chain': {
       url: 'https://api.avax-test.network/ext/bc/C/rpc',
-      chainId: 43113
+      chainId: 43113,
+      accounts: [
+        '0xd02eea6052529458cca873975cf6b226a92282758784a2e06d6733aa45ea47b9'
+      ]
     }
   }
 }

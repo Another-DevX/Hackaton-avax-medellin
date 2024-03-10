5b36c4a321e1559d7943ef02a67d12f5f8547fc0
sebastian_ortiz <sebasor@gmail.com>
2024-03-10 16:05:54 -0500

    Deply

--- packages/smart_contracts/scripts/deploy-hyper-vaults.ts
+++ packages/smart_contracts/scripts/deploy-hyper-vaults.ts
@@ -5,7 +5,7 @@ async function main () {
     '0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf'
   const destinationChainId =
     '0x1278d1be4b987e847be3465940eb5066c4604a7fbd6e086900823597d81af4c1'
-  const destinationAddress = '0x44eb0719ee3eC152b6Cc218C0D97bCC339E2F3C6'
+  const destinationAddress = '0x6B09E65cfCac859d38fc7D629958891A4e9380C2'
   const hyperVayult = await ethers.deployContract('HyperVault', [
     teleporterMessengerAddress,
     destinationChainId,
